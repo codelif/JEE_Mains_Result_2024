53c5fa08833d32bef24121e1b7f28e9c33bf6fff
Harsh Sharma <goharsh007@gmail.com>
2024-02-12 15:03:33 +0530

    prod4

--- marks.py
+++ marks.py
@@ -11,14 +11,31 @@ def get_marks(link: str):
 
     my_answers = serialize_answers(BytesIO(r.content))
 
-    with open("answer_key.json") as fa:
-        answer_key = json.load(fa)
+    with open("answerkeys.json") as fa:
+        for i in json.load(fa):
+            if i["date"] == my_answers["date"] and i["shift"] == my_answers["shift"]:
+                answer_key = i["key"]
+                break
 
+    return raw_marks(my_answers["key"], answer_key)
+
+
+def raw_marks(candidate_answers, answer_key):
     marks = 0
-    for i in my_answers:
-        if int(answer_key[str(i["id"])][0]) == int(i["answer"]):
+    for i in candidate_answers:
+        if "Drop" in answer_key[str(i["id"])]:
             marks += 4
+            continue
+
+        if i["type"] == "MCQ":
+            if str(i["option_id"]) in answer_key[str(i["id"])]:
+                marks += 4
+            else:
+                marks -= 1
         else:
-            marks -= 1
+            if int(answer_key[str(i["id"])][0]) == int(i["answer"]):
+                marks += 4
+            else:
+                marks -= 1
 
     return marks
--- serialize_data.py
+++ serialize_data.py
@@ -1,11 +1,13 @@
-from bs4 import BeautifulSoup as BS
 import json
+
+from bs4 import BeautifulSoup as BS
+
 
 def serialize_key(f):
     soup = BS(f.read(), "lxml")
     f.close()
     key_table = soup.find("table")
-    
+
     answer_key = {}
     for i in key_table.find_all("tr")[1:]:
         row = i.find_all("td")
@@ -13,7 +15,10 @@ def serialize_key(f):
         answer = int(row[1].text.strip())
         answer_type = "MCQ"
 
-        if i.find("input", attrs={"onkeypress":"return blockSpecialChar(event,'alphanumeric')"}):
+        if i.find(
+            "input",
+            attrs={"onkeypress": "return blockSpecialChar(event,'alphanumeric')"},
+        ):
             answer_type = "SA"
 
         answer_key.update({question_id: [answer, answer_type]})
@@ -27,20 +32,32 @@ def serialize_key(f):
 def get_cell_text(tag):
     return tag.find_all("td")[-1].text.strip()
 
+
 def serialize_answers(f):
     soup = BS(f.read(), "lxml")
     f.close()
     my_answers = []
 
-    for question in soup.find_all("table", attrs={"class": "questionPnlTbl"}): 
+    for i in soup.find("table").find_all("tr"):
+        a = i.find_all("td")
+        if a[0].text == "Test Date":
+            test_date = a[1].text.strip().replace("/", "-")
+        elif a[0].text == "Test Time":
+            if a[1].text.strip().startswith("3:00"):
+                shift = "Second"
+            else:
+                shift = "First"
+
+    for question in soup.find_all("table", attrs={"class": "questionPnlTbl"}):
         question_menu = question.find("table", attrs={"class": "menu-tbl"})
-        
+
         question_menu_rows = question_menu.find_all("tr")
         answer_type = get_cell_text(question_menu_rows[0])
 
         question_id = get_cell_text(question_menu_rows[1])
 
         answers = []
+        a = None
         if answer_type == "MCQ":
             for i in question_menu_rows[2:6]:
                 answers.append(int(get_cell_text(i)))
@@ -49,13 +66,11 @@ def serialize_answers(f):
                 my_answer = None
                 continue
             my_answer = int(my_answer)
-            a = answers[my_answer-1]
+            a = answers[my_answer - 1]
 
-            canonical = sorted(answers).index(a)+1
+            canonical = sorted(answers).index(a) + 1
 
             my_answer = canonical
-
-
 
         else:
             question_table = question.find("table", attrs={"class": "questionRowTbl"})
@@ -65,12 +80,18 @@ def serialize_answers(f):
                 my_answer = None
                 continue
 
-        my_answers.append({"id": int(question_id), "type": answer_type, "answer": int(my_answer)})
-    
-    my_answers = sorted(my_answers, key=lambda x: x["id"])
-    
-    return my_answers
+        my_answers.append(
+            {
+                "id": int(question_id),
+                "type": answer_type,
+                "answer": int(my_answer),
+                "option_id": a,
+            }
+        )
 
+    my_answers = sorted(my_answers, key=lambda x: x["id"])
+
+    return {"date": test_date, "shift": shift, "key": my_answers}
 
 
 if __name__ == "__main__":
